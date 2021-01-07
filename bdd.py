e4f5714794cd037d864c0689e95c0b8a10e325ea
ChainYo <thomas.chaigneau@isen-ouest.yncrea.fr>
2021-01-07 20:31:36 +0100

    add json file

--- bdd.py
+++ bdd.py
@@ -1,10 +1,20 @@
 import pymongo
+import os
+import json
+import subprocess
+
+from tkinter import filedialog
 from pprint import pprint
+
 try:
-    from ids import user, passw
+    from ids import user, passw, db
 except:
     user = 'stephane_isen'
     passw = 'isenBrest_29'
+
+# Règle un problème avec la fenetre tkinter sur Ubuntu 20.04
+if os.environ.get('DISPLAY','') == '':
+    os.environ.__setitem__('DISPLAY', ':0.0')
 
 # Classe de la connection à la base de données
 class MongoDB_Log():
@@ -18,7 +28,8 @@ class MongoDB_Log():
     @classmethod
     def open_con(cls):
         cls.client = cls.login()
-        cls.db = cls.client.dblp.publis
+        cls.db = cls.client.dblp
+        cls.collect = cls.db.publis
 
     # Méthode pour fermer la connection à la bdd
     @classmethod
@@ -29,7 +40,7 @@ class MongoDB_Log():
     @classmethod
     def count_docs(cls):
         cls.open_con()
-        data = list(cls.db.find())
+        data = list(cls.collect.find())
         cls.close_con()
         return print(f"Il y a {len(data)} documents dans notre base de données.")
 
@@ -37,7 +48,7 @@ class MongoDB_Log():
     @classmethod
     def list_type(cls):
         cls.open_con()
-        data = list(cls.db.find({'type':'Book'}))
+        data = list(cls.collect.find({'type':'Book'}))
         cls.close_con()
         return pprint(data)
 
@@ -45,7 +56,7 @@ class MongoDB_Log():
     @classmethod
     def list_book_date(cls, date):
         cls.open_con()
-        data = list(cls.db.find({'type':'Book', 'year':{'$gte':date}}, {'_id':0, 'title':1, 'authors':1}))
+        data = list(cls.collect.find({'type':'Book', 'year':{'$gte':date}}, {'_id':0, 'title':1, 'authors':1}))
         cls.close_con()
         return pprint(data)
 
@@ -53,7 +64,7 @@ class MongoDB_Log():
     @classmethod
     def list_publi_auth(cls, auth):
         cls.open_con()
-        data = list(cls.db.find({'authors':auth}, {'_id':0, 'title':1, 'authors':1}))
+        data = list(cls.collect.find({'authors':auth}, {'_id':0, 'title':1, 'authors':1}))
         cls.close_con()
         return pprint(data)
 
@@ -61,7 +72,7 @@ class MongoDB_Log():
     @classmethod
     def list_all_auth(cls):
         cls.open_con()
-        data = list(cls.db.distinct("authors"))
+        data = list(cls.collect.distinct("authors"))
         cls.close_con()
         return pprint(data)
 
@@ -69,7 +80,7 @@ class MongoDB_Log():
     @classmethod
     def list_publi_auth_sort(cls, auth):
         cls.open_con()
-        data = list(cls.db.find({"authors":auth}).sort('title',1))
+        data = list(cls.collect.find({"authors":auth}).sort('title',1))
         cls.close_con()
         return pprint(data)
 
@@ -77,7 +88,7 @@ class MongoDB_Log():
     @classmethod
     def count_publi_auth(cls, auth):
         cls.open_con()
-        data = list(cls.db.find({"authors":auth}))
+        data = list(cls.collect.find({"authors":auth}))
         cls.close_con()
         return print(f"Il y a {len(data)} publications de {auth}.")
 
@@ -85,7 +96,7 @@ class MongoDB_Log():
     @classmethod
     def count_publi_date_type(cls, date):
         cls.open_con()
-        data = list(cls.db.aggregate([{'$match':{"year":{'$gte':date}}}, {'$group':{'_id':'$type','count':{'$sum':1}}}]))
+        data = list(cls.collect.aggregate([{'$match':{"year":{'$gte':date}}}, {'$group':{'_id':'$type','count':{'$sum':1}}}]))
         cls.close_con()
         return pprint(data)
 
@@ -93,12 +104,23 @@ class MongoDB_Log():
     @classmethod
     def count_publi_all_auth(cls):
         cls.open_con()
-        data = list(cls.db.aggregate([{'$unwind':'$authors'}, {'$group' : { '_id' : '$authors', 'count' : {'$sum' : 1}}}, {'$sort':{'count':1}}]))
+        data = list(cls.collect.aggregate([{'$unwind':'$authors'}, {'$group' : { '_id' : '$authors', 'count' : {'$sum' : 1}}}, {'$sort':{'count':1}}]))
         cls.close_con()
         return pprint(data)
 
+    # Méthode pour choisir le fichier json à insérer
+    @classmethod
+    def insert_json(cls, collect_name):
+        cls.open_con()
+        file = filedialog.askopenfilename()
+        name = file.split('/')[-1]
+        subprocess.call(f"mongoimport --uri mongodb+srv://{user}:{passw}@clusterkata.b6v13.mongodb.net/{db} --collection {collect_name} --type json --file {file}", shell=True)
+        cls.close_con()
+        return print(f"Le fichier {name} a bien été inséré dans la collection {collect_name}.")
+
 '''
 Toutes les commandes du brief.
+Voir le résultat dans le notebook de présentation.
 '''
 # MongoDB_Log().count_docs()
 # MongoDB_Log().list_type()
@@ -109,3 +131,4 @@ Toutes les commandes du brief.
 # MongoDB_Log().count_publi_auth("Toru Ishida")
 # MongoDB_Log().count_publi_date_type(2011)
 # MongoDB_Log().count_publi_all_auth()
+# MongoDB_Log.insert_json('nom_de_la_collection')
